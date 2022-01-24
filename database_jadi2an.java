8debe184c900dce0e9cd857bc0eede124cb13ee9
fahrimaulana <fahri_ghazali_30rpl@student.smktelkom-mlg.sch.id>
2022-01-25 06:13:44 +0700

    kak

--- database_jadi2an.java
+++ database_jadi2an.java
@@ -36,6 +36,6 @@ public class database_jadi2an {
     public static void mie_sedap(){
         System.out.println("Nama Produk: Mie Sedap");
         System.out.println("Harga: Rp3000");
-        System.out.println("Stok: 290pcs");
+        System.out.println("Stok: 280pcs");
     }
 }
